576716133ca96264c1ab5fa8e54be853c9bc11da
Vinay Soni <98009479+VinaySoni04@users.noreply.github.com>
2023-05-10 18:08:31 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,14 +1,5 @@
 package com.driver;
 
 public class Main {
-    public static void main(String[] args) {
-        RWOnly obj = new RWOnly();
-        obj.name = "John"; // Error: The field RWOnly.name is not accessible
-        System.out.println(obj.name); // Error: The field RWOnly.name is not accessible
-        // Because we can't access private member from outside the class
-
-        // Setting and Getting the name using setter and getter function
-        obj.setName("Vinay");
-        System.out.println(obj.getName());
-    }
+    
 }
