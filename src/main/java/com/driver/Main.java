ac39eefdeec8d846a778f1a0d9f7d3ec0c1947e3
Vinay Soni <vinay042801@gmail.com>
2023-05-10 18:17:28 +0530

    Implemented the Main and RWOnly class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        obj.name = "John"; // Error: name has private access in com.driver.RWOnly
+        System.out.println(obj.name); // Error: name has private access in com.driver.RWOnly
+        // Because we can't access the private member from outside the class
+
+        obj.setName("Vinay Soni");
+        System.out.println(obj.getName());
+    }
 }
